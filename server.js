e57a2c881c9bcac53a20834d8efa8331ba31bae0
IdrisLaabidi <idrislaabidi@gmail.com>
2024-02-28 17:05:04 +0100

    completing backend file structure

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json()) // instead of using body parser 
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+//put routes here
+
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then( () => {
+  console.log("connected to db");
+  // listen for requests
+  app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+  })
+ }
+)
+.catch(
+  (err) => console.log(err)
+);
