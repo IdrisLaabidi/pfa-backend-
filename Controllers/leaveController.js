9aa4ddda8bba0bd84dca9d1855af9d0225609fc9
tasnimebbk <tasnime@gmail.com>
2024-03-04 19:25:28 +0100

    added leave routes and controller

---
+++ Controllers/leaveController.js
@@ -0,0 +1,73 @@
+// Import necessary modules
+const Leave = require('../Models/leaveModel'); // Import your Leave model
+
+// Create a new leave request
+const createLeaveRequest = async (req, res) => {
+  try {
+    const newLeaveRequest = await Leave.create(req.body);
+    res.status(201).json(newLeaveRequest);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Get all leave requests
+const getAllLeaveRequests = async (req, res) => {
+  try {
+    const leaveRequests = await Leave.find();
+    res.json(leaveRequests);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get a specific leave request by ID
+const getLeaveRequestById = async (req, res) => {
+  try {
+    const leaveRequest = await Leave.findById(req.params.id);
+    if (!leaveRequest) {
+      return res.status(404).json({ message: 'Leave request not found' });
+    }
+    res.json(leaveRequest);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update a leave request by ID
+const updateLeaveRequestById = async (req, res) => {
+  try {
+    const updatedLeaveRequest = await Leave.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedLeaveRequest) {
+      return res.status(404).json({ message: 'Leave request not found' });
+    }
+    res.json(updatedLeaveRequest);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Delete a leave request by ID
+const deleteLeaveRequestById = async (req, res) => {
+  try {
+    const deletedLeaveRequest = await Leave.findByIdAndDelete(req.params.id);
+    if (!deletedLeaveRequest) {
+      return res.status(404).json({ message: 'Leave request not found' });
+    }
+    res.json({ message: 'Leave request deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  createLeaveRequest,
+  getAllLeaveRequests,
+  getLeaveRequestById,
+  updateLeaveRequestById,
+  deleteLeaveRequestById,
+};
