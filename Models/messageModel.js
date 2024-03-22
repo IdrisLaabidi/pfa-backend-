56bec5489cd9561dc5ed03ff5c62e3b99546327c
RayenMiri <rayenmiri000@gmail.com>
2024-03-22 16:43:20 +0100

    changed the messages model

--- Models/messageModel.js
+++ Models/messageModel.js
@@ -7,6 +7,7 @@ const MessageSchema = new mongoose.Schema({
     },
     sender : {type : String, ref: 'User'},
     sentTo : [{ type : String, ref: 'User'}],
+    who :{type : String} ,
     //for the moment it's an array of  strings later it will be an array of  mongoose.Schema.Types.ObjectId
     project : {type: mongoose.Schema.Types.ObjectId, ref: 'Project'},
     sentAt : {type : Date , default : Date.now}
